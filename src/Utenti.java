bb09219629935bada992e338bd05ce9f82372b52
UsrWthoutName <gabry21102002@gmail.com>
2022-04-19 11:37:59 +0200

    Aggiunta directory parziale della classe Utenti

--- src/Utenti.java
+++ src/Utenti.java
@@ -1,22 +1,5 @@
 import java.io.*;
 
 public class Utenti {
-    
-    int id;
-    String nome;
-    String codicefiscale;
-    String indirizzo;
-    String email;
-    String password;
-
-    public Utenti(String nome, String codiceFiscale, String indirizzo, String email, String password){   
-        
-    }
-    public Utenti(String email, String password){ //Chiama login
-        Login(); 
-    }
-    public void Registrazione(){
-        id = 1; //Apre lettura file e legge id ultimo utente presente +1
-        
-    }        
+  
 }
