2517c03c6b082848ba0d69539c88007e9b53c6dd
UsrWthoutName <gabry21102002@gmail.com>
2022-04-21 12:17:45 +0200

    classe Utenti completata con metodo Login

--- src/Utenti.java
+++ src/Utenti.java
@@ -23,6 +23,45 @@ public class Utenti {
         password = pw;
         Registrazione();
     }
+    public Utenti(String cred, String pass){ // Credenziale può essere username o email
+    
+        Login(cred, pass);
+    }
+    private void Login(String cred, String pass){
+        File f = new File(path + "UtentiRegistrati.dati.csv");
+        String s;
+        String[] s_Split; 
+        Boolean eseguito = false;
+        try {
+        
+            Scanner file = new Scanner(f);
+            while(file.hasNextLine() == true){
+                s = file.nextLine();
+                s_Split = s.split(";");
+                if(eseguito == false){
+                    if(s_Split[4].equals(cred) || s_Split[6].equals(cred)){
+                        if(s_Split[7].equals(pass)){
+                            System.out.println("Login eseguito");
+                            eseguito = true;
+                            userid = Integer.parseInt(s_Split[0]);
+                            nome = s_Split[1];
+                            cognome = s_Split[2];
+                            codfis = s_Split[3];
+                            username = s_Split[4];
+                            indirizzo = s_Split[5];
+                            email = s_Split[6];
+                            password = s_Split[7];
+                        }
+                    }
+                }
+            }
+            if(eseguito == false){
+                System.out.println("Credenziali non valide");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
     private void Registrazione(){
         File f = new File(path + "UtentiRegistrati.dati.csv");
         try {
