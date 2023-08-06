c22bc3303790727f8280dc394b12290f2aedc909
Sind01 <102220572+Paperino01@users.noreply.github.com>
2023-08-06 19:46:28 +0200

    Update HelloApplication.java

--- src/main/java/com/example/emotionalsongserver/HelloApplication.java
+++ src/main/java/com/example/emotionalsongserver/HelloApplication.java
@@ -7,8 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * The HelloApplication class represents a JavaFX application which uses a GUI, loading the "ServerStart.fxml" file, using FXMLLoader and setting up the GUI scene on the stage.
+ **/
 public class HelloApplication extends Application {
     @Override
+     /**
+     * The start method  loads the "ServerStart.fxml" file, creates and sets the scene on the stage, and displays it.
+     *
+     * @param stage is the primary stage for the application.
+     * @throws IOException if an I/O error occurs while loading the FXML file.
+     **/
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ServerStart.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 651, 605);
