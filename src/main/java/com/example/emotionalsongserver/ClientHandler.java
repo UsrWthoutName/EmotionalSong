b275cd5c91bb2334b8e9ea6d87839b836a4eb8ff
UsrWthoutName <gabry21102002@gmail.com>
2023-08-29 18:24:00 +0200

    server di merda finito

--- src/main/java/com/example/emotionalsongserver/ClientHandler.java
+++ src/main/java/com/example/emotionalsongserver/ClientHandler.java
@@ -64,7 +64,6 @@ public class ClientHandler implements Runnable {
                                 out.println("-1");  //no corrispondenza credenziale-password
                             }
                             conn.close();
-                            System.out.println("login terminato");
                         }catch (Exception e){}
                     break;
                 case "R":   //REGISTRAZIONE
@@ -105,7 +104,6 @@ public class ClientHandler implements Runnable {
                             res.next();
                             //AMAZEMENT~SOLEMNITY~TENDERNESS~NOSTALGIA~CALMNESS~POWER~JOY~TENSION~SADNESS
                             String srvres = res.getString("amAVG")+"~"+res.getString("solAVG")+"~"+res.getString("tendAVG")+"~"+res.getString("nosAVG")+"~"+res.getString("calAVG")+"~"+res.getString("powAVG")+"~"+res.getString("joyAVG")+"~"+res.getString("tensAVG")+"~"+ res.getString("sadAVG");
-                            System.out.println(srvres);
                             out.println(srvres);
 
                             int c = 0;
@@ -126,7 +124,6 @@ public class ClientHandler implements Runnable {
                             }
                             else{
                                 srvres = srvres.substring(0, srvres.length()-1);
-                                System.out.println(srvres);
                                 //EMOZIONE_N~RECENSIONE~...
                                 out.println(srvres);
                             }
@@ -168,7 +165,7 @@ public class ClientHandler implements Runnable {
                     break;
                 case "P":   //PLAYLIST
                         out.println("C");
-                        s = in.readLine();  //s contiene ID utente
+                        s = in.readLine();  //contiene ID utente
                         try {
                             Connection conn = DriverManager.getConnection(url, username, password);
                             Statement statement = conn.createStatement();
@@ -208,10 +205,176 @@ public class ClientHandler implements Runnable {
                             System.err.println(e);
                         }
                     break;
-                case "V":   //VALUTAZIONE
+                case "V":   //VALUTAZIONE pt1 - ritorna nomi canzoni partendo da id playlist
+                    out.println("C");
+                    s = in.readLine(); //id playlist
+                    String idplaylist = s;
+                    try {
+                        Connection conn = DriverManager.getConnection(url, username, password);
+                        Statement statement = conn.createStatement();
+                        q = "SELECT idcanzone FROM emozioni WHERE idplaylist = '"+idplaylist+"'";
+                        ResultSet res = statement.executeQuery(q);
+                        String srvres = "";
+                        ArrayList <String> idcanz = new ArrayList<String>();
+                        while (res.next()){
+                            idcanz.add(res.getString("idcanzone"));
+                        }
+                        int l = idcanz.size();
+                        int i = 0;
+                        while (i<l){
+                            q = "SELECT titolo FROM canzoni WHERE id='"+idcanz.get(i)+"'";
+                            res = statement.executeQuery(q);
+                            res.next();
+                            srvres = srvres+idcanz.get(i)+"~"+res.getString("titolo")+"~";
+                            i++;
+                        }
+                        srvres = srvres.substring(0, srvres.length()-1);
+                        System.out.println(srvres);
+                        out.println(srvres);
+
+                    }catch (Exception e){
+                        System.out.println(e);
+                    }
                     break;
-                case "I":   //VISUALIZZAZIONE ELEMENTI IN PLAYLIST
+                case "Vb": //VALUTAZIONE pt2 - permette di valutare una playlist
+                    try {
+                        Connection conn = DriverManager.getConnection(url, username, password);
+                        Statement statement = conn.createStatement();
+                        out.println("C");
+                        s = in.readLine();
+                        String[] val = s.split("~");
+
+                        //IDPLAYLIST ~ CANZONE ~ CANZONE ~ CANZONE
+                        // CANZONE = IDCANZONE ~ EMOZIONE1 VALUTAZIONE ~ EMOZIONE1 RECENSIONE ~ EMOZIONE2 VALUTAZIONE ~ EMOZIONE2 RECENSIONE
+                        int len = val.length;
+                        int numerocanzoni = (len - 1)/19;
+
+                        int i=0;
+                        idplaylist = val[i];
+                        i++;
+                        String emozioni[] = {"amazement", "solemnity", "tenderness", "nostalgia", "calmness", "power","joy", "tension", "sadness"};
+                        while (i<len){
+                            String idcanzone = val[i];
+                            i++; //i corrsiponde a primo elemento in blocco canzone
+                            int j = 0;
+                            while (j<9){
+                                q = "UPDATE emozioni SET "+emozioni[j]+" = "+val[i]+" WHERE idplaylist="+idplaylist+" AND idcanzone = '"+idcanzone+"'";
+                                System.out.println(q);
+                                statement.executeUpdate(q);
+                                i++;
+                                //controllo se stringa null
+                                if (!val[i].equals("null")){
+                                    q = "UPDATE emozioni SET "+emozioni[j]+"_n = '"+val[i]+"' WHERE idplaylist="+idplaylist+" AND idcanzone = '"+idcanzone+"'";
+                                    System.out.println(q);
+                                    statement.executeUpdate(q);
+                                }
+                                i++;
+                                j++;
+                            }
+                            q = "UPDATE emozioni SET valutata = true WHERE idplaylist="+idplaylist+" AND idcanzone = '"+idcanzone+"'";
+                            statement.executeUpdate(q);
+                        }
+
+                    }catch (Exception e){
+                        System.out.println(e);
+                    }
+
                     break;
+                case "I":   //INSERIMENTO ELEMENTI IN PLAYLIST
+                        out.println("C");
+                        s = in.readLine();
+                        String[] el = s.split("~");
+
+                        try {
+                            Connection conn = DriverManager.getConnection(url, username, password);
+                            Statement statement = conn.createStatement();
+                            //verificare che canzone non sia presete in playlist
+                            q = "SELECT COUNT(*) FROM emozioni WHERE idplaylist = "+el[0]+" AND idcanzone = '"+el[1]+"'";
+                            ResultSet res = statement.executeQuery(q);
+                            res.next();
+                            if (res.getString("COUNT").equals("0")) {
+                                q = "INSERT INTO emozioni (idplaylist, idcanzone, valutata) VALUES (" + el[0] + ",'" + el[1] + "',false)";
+                                statement.executeUpdate(q);
+                                out.println("1");
+                            }else {
+                                out.println("-1");
+                            }
+                        }catch (Exception e){
+                            System.out.println(e);
+                        }
+                    break;
+                case "A":   //LETTURA CANZONI IN PLAYLIST
+                        out.println("C");
+                        s = in.readLine(); //ID PLAYLIST
+                    try {
+                        Connection conn = DriverManager.getConnection(url, username, password);
+                        Statement statement = conn.createStatement();
+                        q = "SELECT valutata FROM emozioni WHERE idplaylist = '"+s+"'";
+                        ResultSet res = statement.executeQuery(q);
+                        if (res.next()){
+                            out.println(res.getString("valutata"));
+                            boolean val;
+                            if (res.getString("valutata").equals("t")){
+                                val = true;
+                            }else {
+                                 val = false;
+                            }
+                            String srvres = "";
+                            ArrayList<String> ALid = new ArrayList<String>();
+                            ArrayList<String> ALinfo = new ArrayList<String>();
+                            ArrayList<String> ALval = new ArrayList<String>();
+
+                            q="SELECT idcanzone FROM emozioni WHERE idplaylist='"+s+"'";
+                            res = statement.executeQuery(q);
+                            while (res.next()){
+                                ALid.add(res.getString("idcanzone"));
+                            }
+                            int ncanz = ALid.size();
+                            int i = 0;
+                            while (i<ncanz){
+                                q = "SELECT * FROM canzoni WHERE id = '"+ ALid.get(i)+"'";
+                                res = statement.executeQuery(q);
+                                res.next();
+                                srvres = srvres+res.getString("titolo")+"~"+res.getString("autore")+"~"+res.getString("anno")+"~";
+                                if (val){
+                                    q="SELECT * FROM emozioni WHERE idplaylist = "+s+" AND idcanzone = '"+ALid.get(i)+"'";
+                                    res = statement.executeQuery(q);
+                                    res.next();
+                                    srvres = srvres+res.getString("amazement")+"~"+res.getString("solemnity")+"~"+res.getString("tenderness")+"~"+res.getString("nostalgia")+"~"+res.getString("calmness")+"~"+res.getString("power")+"~"+res.getString("joy")+"~"+res.getString("tension")+"~"+res.getString("sadness")+"~";
+                                }
+                                i++;
+                            }
+                            srvres =  srvres.substring(0, srvres.length()-1);
+                            out.println(srvres);
+                        }else {
+                            out.println("-1");
+                        }
+                    }catch (Exception e){
+                        System.out.println(e);
+                    }
+                    break;
+                case "B":   //CREA PLAYLIST
+                        out.println("C");
+                        s = in.readLine();  //IDUTENTE~NOMEPLAYLIST
+                        el = s.split("~");
+                        try {
+                            Connection conn = DriverManager.getConnection(url, username, password);
+                            Statement statement = conn.createStatement();
+                            q = "SELECT COUNT(*) AS num FROM playlist WHERE possessore="+el[0]+" AND nome = '"+el[1]+"'";
+                            ResultSet res = statement.executeQuery(q);
+                            res.next();
+                            if (!res.getString("num").equals("0")) {
+                                out.println("-1");
+                            }else{
+                                q = "INSERT INTO playlist (nome, possessore) VALUES ('"+el[1]+"',"+el[0]+")";
+                                statement.executeUpdate(q);
+                                out.println("1");
+                            }
+                        }catch (Exception e){
+                            System.out.println(e);
+                        }
+                    break;
+
             }
 
         } catch (IOException e) {
